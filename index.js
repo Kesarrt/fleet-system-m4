a6fb5ca032b098c88e16a6f8afe90f17b66c6a83
Kesarrt <tandulkarkhushi@gmail.com>
2026-01-31 21:10:57 +0530

    index

--- index.js
+++ index.js
@@ -13,3 +13,7 @@ app.use("/users",require("./routes/user.routes"));
 app .listen(process.env.PORT,()=>{
     console.log('Server running on port ${process.env.PORT}');
 });
+
+const notFound =required("./middlewares/notFound.middleware"
+);
+app.use(notFound);
